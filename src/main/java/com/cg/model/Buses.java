d10493a756b9f2ef433f2f7290849378712ed829
Shreshth-gupta <shreshthagupta79@gmail.com>
2026-03-25 15:22:10 +0530

    Added Buses model

---
+++ src/main/java/com/cg/model/Buses.java
@@ -0,0 +1,25 @@
+package com.cg.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Table(name = "buses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Buses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer busId;
+    private Integer officeId;
+    private String registrationNumber;
+    private Integer capacity;
+    private String type;
+}
