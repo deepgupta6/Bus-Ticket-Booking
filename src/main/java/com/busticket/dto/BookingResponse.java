729810f4967099b166e48cadf29c254055b4fbc6
javed <ijavedofficial@gmail.com>
2026-03-27 15:53:08 +0530

    Added Agency, AgencyOffice, booking, bus and customer DTO

--- src/main/java/com/busticket/dto/BookingResponse.java
+++
@@ -1,4 +0,0 @@
-package com.busticket.dto;
-
-public class BookingResponse {
-}
---
+++ src/main/java/com/busticket/dto/BusDTO.java
@@ -0,0 +1,15 @@
+package com.busticket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BusDTO {
+
+    private Integer busId;
+    private Integer officeId;   // AgencyOffice ka ID
+    private String registrationNumber;
+    private Integer capacity;
+    private String type;
+}
