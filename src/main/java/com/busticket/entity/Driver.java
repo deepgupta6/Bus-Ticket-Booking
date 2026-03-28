d520d966824936b4cb13b4d8e1f7053fa9f34810
Deep Gupta <deep2004gupta@gmail.com>
2026-03-28 14:40:03 +0530

    added all repository interfaces

--- src/main/java/com/busticket/entity/Driver.java
+++ src/main/java/com/busticket/entity/Driver.java
@@ -3,14 +3,16 @@ package com.busticket.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "drivers")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Driver {
 
 	@Id
--- src/main/java/com/busticket/respository/BookingRepo.java
+++
@@ -1,5 +0,0 @@
-package com.busticket.respository;
-
-public class BookingRepo {
-}
-
