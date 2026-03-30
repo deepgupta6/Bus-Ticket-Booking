0b3ac2272bb3bcc1f2f1c82630fcdf799c1d6754
javed <ijavedofficial@gmail.com>
2026-03-30 12:25:29 +0530

    Imports

--- src/main/java/com/busticket/respository/ITripRepo.java
+++ src/main/java/com/busticket/respository/ITripRepo.java
@@ -1,8 +1,12 @@
 package com.busticket.respository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.busticket.entity.Trip;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ITripRepo extends JpaRepository<Trip, Integer>{
 
@@ -10,7 +14,6 @@ public interface ITripRepo extends JpaRepository<Trip, Integer>{
 	List<Trip> findByRoute_RouteId(Integer routeId);
 
     @EntityGraph(attributePaths = {"bus", "route"})
-
     Optional<Trip> findByTripId(Long tripId);
 }
 
