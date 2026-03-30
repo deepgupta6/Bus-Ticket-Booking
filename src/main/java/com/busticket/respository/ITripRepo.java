a1ce6f626aba2ae3bc614852563d378102125b3c
Ayush <chaudharyayush028@gmail.com>
2026-03-30 10:58:16 +0530

    Added Get API forgetting routes with all the trips using a routeid.

--- src/main/java/com/busticket/respository/ITripRepo.java
+++ src/main/java/com/busticket/respository/ITripRepo.java
@@ -1,9 +1,13 @@
 package com.busticket.respository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.busticket.entity.Trip;
 
 public interface ITripRepo extends JpaRepository<Trip, Integer>{
 
+	
+	List<Trip> findByRoute_RouteId(Integer routeId);
 }
