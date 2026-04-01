e02de302360d70f9f7914ddcb044b4fda45c2b93
Shreshth-gupta <shreshthagupta79@gmail.com>
2026-04-01 16:04:21 +0530

    added exception for delete

--- src/main/java/com/busticket/respository/IBookingRepo.java
+++ src/main/java/com/busticket/respository/IBookingRepo.java
@@ -2,10 +2,12 @@ package com.busticket.respository;
 
 import com.busticket.entity.Booking;
 import com.busticket.entity.enums.BookingStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
@@ -16,8 +18,10 @@ public interface IBookingRepo extends JpaRepository<Booking, Integer> {
 
 
     List<Booking> findByTrip_TripIdAndStatus(Integer id, BookingStatus status);
+
     @Query("SELECT b, p FROM Booking b LEFT JOIN Payment p ON p.booking.bookingId = b.bookingId")
     List<Object[]> findAllWithPayment();
+
     @Query("SELECT b, p FROM Booking b LEFT JOIN Payment p ON p.booking.bookingId = b.bookingId WHERE b.trip.tripId = :id")
     List<Object[]> findByTripIdWithPayment(@Param("id") Integer id);
 
